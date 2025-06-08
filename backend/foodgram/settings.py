2daa9a1acc2976b64a6cdf3cddbcf58afbb273e3
SemRepin <crash3008@gmail.com>
2025-06-08 18:21:05 +0500

    Fixed backend static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -107,7 +107,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "collected_static"
+STATIC_ROOT = "/app/collected_static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "/media"
